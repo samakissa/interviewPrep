755db0d761b540c0799f1aab84dae66ae2d81b52
Issa Samake <samakissa2002@gmail.com>
2021-11-15 23:01:46 -0600

    added P226 solution

---
+++ BFS/P226.py
@@ -0,0 +1,27 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def invertTree(root: TreeNode) -> TreeNode:
+    if root is None:
+        return None
+    root.right, root.left= invertTree(root.left), invertTree(root.right)
+    return root
+
+def creatBTree(data, index):
+    pNode = None
+    if index < len(data):
+        if data[index] == None:
+            return
+        pNode = TreeNode(data[index])
+        pNode.left = creatBTree(data, 2 * index + 1) # [1, 3, 7, 15, ...]
+        pNode.right = creatBTree(data, 2 * index + 2) # [2, 5, 12, 25, ...]
+    return pNode 
+
+root = creatBTree([4,2,7,1,3,6,9], 0)
+
+invertTree(root)
+
+print(root.left.val)
